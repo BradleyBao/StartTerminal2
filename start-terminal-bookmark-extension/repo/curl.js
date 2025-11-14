a4d84aa60813c8c9f52f255bb9a9287fbd24473c
Tian Yi <tianyi@tianyibrad.com>
2025-11-14 18:08:24 +0900

    Create curl.js

---
+++ start-terminal-bookmark-extension/repo/curl.js
@@ -0,0 +1,16 @@
+// curl.js
+const url = args[0];
+if (!url) {
+    st_api.writeHtml('<span class="term-error">Usage: curl <url></span>');
+    return;
+}
+
+try {
+    st_api.writeLine(`Fetching ${url}...`);
+    const response = await fetch(url, { cache: 'no-store' });
+    const text = await response.text();
+    st_api.writeLine(text); // 打印纯文本响应
+} catch(e) {
+    st_api.writeHtml(`<span class="term-error">${e.message}</span>`);
+    st_api.writeHtml(`<span class="term-error">Did you 'sudo apt install curl' to grant <all_urls> permission?</span>`);
+}
